37b0b2aa8dd57ff278b71c1774eeec4a9c38bc1e
MAAZALAMGIR <159412290+MAAZALAMGIR@users.noreply.github.com>
2024-02-16 14:02:55 +0530

    Add files via upload

---
+++ clasum.java
@@ -0,0 +1,14 @@
+class clasum
+{
+ public static void main(String args[])
+{
+ String a,b;
+int x,y,z;
+a=args[0];
+b=args[1];
+x=Integer.parseInt(a);
+y=Integer.parseInt(b);
+z=x+y;
+System.out.println("sum="+c);
+}
+}
